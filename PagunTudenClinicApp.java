6da70282e47674b13505d7edb780973ab4832582
reytdn <reynaldotuden88@gmail.com>
2026-02-05 15:00:20 +0800

    TASK TA

---
+++ PagunTudenClinicApp.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PagunTudenClinicApp{
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        String fname = input.nextLine();
+
+        System.out.print("Enter Middle Name: ");
+        String mname = input.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        String lname = input.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = input.nextInt();
+
+        System.out.print("Enter Sex (F/M): ");
+        String sex = input.nextLine();
+
+        PatientInfo Info = new PatientInfo(fname, mname, lname, age, sex);
+    }
+}
---
+++ PatientInfo.java
@@ -0,0 +1,24 @@
+public class PatientInfo {
+    private String fname;
+    private String mname;
+    private String lname;
+    private int age;
+    private String sex; 
+
+    public Patient(String fname, String mname, String lname, int age, String sex) {
+        this.fname = fname;
+        this.mname = mname;
+        this.lnamename = lname;
+        this.age = age;
+        this.sex = sex;
+    }
+    public String getFName() { return fname; }
+    public String getMName() { return mname; }
+    public String getLName() { return lname; }
+    public int getAge() { return age; }
+    public String getSex() { return sex; }
+
+    public String toString() {
+        return "Patient: " + fname + mname + lname + ", Age: " + age + ", Sex: " + sex;
+    }
+}
