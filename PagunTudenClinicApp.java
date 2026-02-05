f2d74aa26b14180df75571cc710f594cd475c859
reytdn <reynaldotuden88@gmail.com>
2026-02-05 15:46:23 +0800

    TASK

--- PagunTudenClinicApp.java
+++ PagunTudenClinicApp.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class PagunTudenClinicApp{
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter First Name: ");
@@ -18,10 +18,28 @@ public class PagunTudenClinicApp{
         int age = input.nextInt();
 
         System.out.print("Enter Sex (F/M): ");
+        input.nextLine(); 
         String sex = input.nextLine();
 
-        PatientInfo Info = new PatientInfo(fname, mname, lname, age, sex);
+        System.out.print("Enter Address: ");
+        String address = input.nextLine();
 
-        input.close();
+        System.out.print("Enter City: ");
+        String city = input.nextLine();
+
+        System.out.print("Enter Province: ");
+        String province = input.nextLine();
+
+        System.out.print("Enter Time of Last Meal: ");
+        String lastMeal = input.nextLine();
+
+        System.out.print("Enter Requesting Physician: ");
+        String physician = input.nextLine();
+
+        PatientInfo info = new PatientInfo(fname, mname, lname, age, sex,
+                address, city, province, lastMeal, physician);
+
+        System.out.println("===== PATIENT INFORMATION =====");
+        System.out.println(info);
     }
 }
--- PatientInfo.java
+++ PatientInfo.java
@@ -1,24 +1,63 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class PatientInfo {
+    private static int nextID = 1001;
+
+    private int patientID;
     private String fname;
     private String mname;
     private String lname;
     private int age;
-    private String sex; 
+    private String sex;
 
-    public PatientInfo(String fname, String mname, String lname, int age, String sex) {
+    private String address;
+    private String city;
+    private String province;
+
+    private String dateOfCollection;
+    private String timeOfCollection;
+
+    private String timeOfLastMeal;
+    private String requestingPhysician;
+
+    public PatientInfo(String fname, String mname, String lname, int age, String sex,
+                       String address, String city, String province,
+                       String timeOfLastMeal, String requestingPhysician) {
+
+        this.patientID = nextID++; 
+
         this.fname = fname;
         this.mname = mname;
         this.lname = lname;
         this.age = age;
         this.sex = sex;
+
+        this.address = address;
+        this.city = city;
+        this.province = province;
+
+        this.dateOfCollection = LocalDate.now().toString();
+
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("hh:mm a");
+        this.timeOfCollection = LocalTime.now().format(timeFormat);
+
+        this.timeOfLastMeal = timeOfLastMeal;
+        this.requestingPhysician = requestingPhysician;
     }
-    public String getFName() { return fname; }
-    public String getMName() { return mname; }
-    public String getLName() { return lname; }
-    public int getAge() { return age; }
-    public String getSex() { return sex; }
 
     public String toString() {
-        return "Patient: " + fname + mname + lname + ", Age: " + age + ", Sex: " + sex;
+        return "Patient ID: " + patientID +
+               "Patient Name: " + fname + " " + mname + " " + lname +
+               "Age: " + age +
+               "Sex: " + sex +
+               "Address: " + address +
+               "City: " + city +
+               "Province: " + province +
+               "Date of Collection: " + dateOfCollection +
+               "Time of Collection: " + timeOfCollection +
+               "Time of Last Meal: " + timeOfLastMeal +
+               "Requesting Physician: " + requestingPhysician;
     }
 }
